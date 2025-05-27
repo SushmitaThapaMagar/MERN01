98789268fb20e9b2027bd48313584f3a207214b2
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-05-27 13:56:29 +0545

    add

---
+++ TypeScript/index.js
@@ -0,0 +1,5 @@
+//tsc is an interpretor 
+var x = 10;
+var x1 = 'Sushmita';
+console.log(x);
+console.log(x1);
---
+++ TypeScript/index.ts
@@ -0,0 +1,9 @@
+//tsc is an interpretor 
+//it should be always converted into js file incase of ts file
+//TS should be converted into JS file
+
+let x:number = 10;
+let x1:String = 'Sushmita';
+console.log(x)
+console.log(x1)
+
